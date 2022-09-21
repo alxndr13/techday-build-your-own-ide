e742c6bae273fd818e2c59884189b95ec3f1daf4
Alexander Huck <alexander.huck@inovex.de>
2022-09-21 11:14:13 +0200

    fix: hybridlinenumbers, tabstop size and leader key

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,4 @@
-vim.opt.number = true           -- Show line number
+vim.opt.relativenumber = true   -- Show hybrid line numbers
 vim.opt.showmatch = true        -- Highlight matching parenthesis
 vim.opt.foldmethod = 'marker'   -- Enable folding (default 'foldmarker')
 vim.opt.colorcolumn = '80'      -- Line lenght marker at 80 columns
@@ -10,11 +10,13 @@ vim.opt.linebreak = true        -- Wrap on word boundary
 vim.opt.termguicolors = true    -- Enable 24-bit RGB colors
 vim.opt.laststatus=3            -- Set global statusline
 vim.opt.expandtab = true        -- Use spaces instead of tabs
-vim.opt.shiftwidth = 4          -- Shift 4 spaces when tab
-vim.opt.tabstop = 4             -- 1 tab == 4 spaces
+vim.opt.shiftwidth = 2          -- Shift 2 spaces when tab
+vim.opt.tabstop = 2             -- 1 tab == 2 spaces
 vim.opt.smartindent = true      -- Autoindent new lines
 vim.opt.hidden = true           -- Enable background buffers
 vim.opt.history = 100           -- Remember N lines in history
 vim.opt.lazyredraw = true       -- Faster scrolling
+vim.opt.undofile = true         -- enable persistent undo
 vim.opt.synmaxcol = 240         -- Max column for syntax highlight
 vim.opt.updatetime = 700        -- ms to wait for trigger an event
+vim.g.mapleader = ' '           -- setting the leader to space
